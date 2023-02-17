77e17cb8b0a287a55fe81d1e38a69fa761f78fd8
iaguest <i.guest2010@gmail.com>
2023-02-17 20:47:14 +0000

    optional, default and child prop usage

--- src/Question.tsx
+++ src/Question.tsx
@@ -3,11 +3,20 @@ import { QuestionData } from './QuestionData';
 
 interface Props {
   data: QuestionData;
+  showContent?: boolean;
 }
 
-export const Question = ({ data }: Props) => (
+export const Question = ({ data, showContent = true }: Props) => (
   <div>
     <div>{data.title}</div>
+    {showContent && (
+      <div>
+        {data.content.length > 50
+          ? `${data.content.substring(0, 50)}...`
+          : data.content}
+      </div>
+    )}
+
     <div>
       {`Asked by ${
         data.userName
